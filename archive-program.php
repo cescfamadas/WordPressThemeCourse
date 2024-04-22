8ae586698aa13bbd5e366827ed6672a772d634d1
francesc famadas <kiski97@gmail.com>
2024-04-22 17:55:06 +0200

    theme sections  done

--- archive-program.php
+++ archive-program.php
@@ -1,16 +1,11 @@
 <?php
 
-get_header(); ?>
-
-<div class="page-banner">
-    <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
-    <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">All programs </h1>
-        <div class="page-banner__intro">
-            <p>something for everyone</p>
-        </div>
-    </div>
-</div>
+get_header();
+pageBanner(array(
+    'title' => 'All Programs',
+    'subtitle' => 'There is something for everyone'
+));
+?>
 
 <div class="container container--narrow page-section">
     <ul class="link-list min-list">
